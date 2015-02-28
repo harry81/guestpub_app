029d95344cee18f8cf3af41b03d43b8f98dfea2b
Hyunmin Choi <pointer81@gmail.com>
2015-02-28 15:19:32 +0900

    Done basic layout without communicating

--- www/js/app.js
+++ www/js/app.js
@@ -39,11 +39,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
-    url: "/browse",
+  .state('app.inquiry', {
+    url: "/inquiry",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "templates/inquiry.html"
       }
     }
   })
